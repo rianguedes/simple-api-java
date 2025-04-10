fee55613d21ba8f2fb8435302899cbc24611619b
Rian Guedes <rianguedes.rd@gmail.com>
2025-04-10 20:35:01 -0300

    Criando meu primeiro Workflow

--- src/test/java/com/github/fiap/simple_api_java/controller/ProdutoControllerTest.java
+++ src/test/java/com/github/fiap/simple_api_java/controller/ProdutoControllerTest.java
@@ -30,34 +30,33 @@ public class ProdutoControllerTest {
 
     private ProdutoCreateDto produtoCreateDto;
 
-    
     @BeforeEach
     public void setup() {
         produtoCreateDto = new ProdutoCreateDto();
         produtoCreateDto.setNome("Produto novo");
 
-        }
-    
+    }
+
     @DisplayName("test create produto")
     @Test
-    void testGivenNewProduto_whenCreate_thenSavedProduto() throws Exception{
-        
+    void testGivenNewProduto_whenCreate_thenSavedProduto() throws Exception {
+
         given(service.saveOrUpdate(any(Produto.class)))
-            .willAnswer((invocation) -> invocation.getArguments()[0]);
+                .willAnswer((invocation) -> invocation.getArguments()[0]);
 
         String body = "{\"nome\":\"Maçã\"}";
-        
+
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/produtos")
-            .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
-            .content(body));
+                .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                .content(body));
 
         response.andExpect(status().isOk());
     }
 
     @DisplayName("test fail")
     @Test
-    void testGivenNewProduto_whenCreate_thenFail() throws Exception{
+    void testGivenNewProduto_whenCreate_thenFail() throws Exception {
         // fail("Um erro acontecerá");
     }
-    
+
 }
